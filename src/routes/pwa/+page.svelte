8598391b271e867c1104aab98f6a3c61ba71f56f
Price <price.comstock@gmail.com>
2025-10-25 14:21:21 -0500

    pwa info and formatting

---
+++ src/routes/pwa/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Offline - FIST Reference</title>
+</svelte:head>
+
+<div class="prose">
+	<h1>P.W.A. Info</h1>
+	<p>
+		FIST Ref is now a P.W.A., or Progressive Web Application. It still works completely as a normal
+		website, but with a few benefits. Data is stored locally on your device, and things should be
+		much snappier after the initial load. This also saves data by not repeatedly requesting it from
+		the server.
+	</p>
+	<p>
+		It also allows offline usage, which should help those of you playing FIST in basements or on
+		abandoned oil rigs.
+	</p>
+	<p>
+		You can also now install it to use as a desktop or mobile app, if you want a dedicated window
+		without the browser UI.
+	</p>
+	<p><b>DESKTOP:</b> Use the install icon in the address bar.</p>
+	<p>
+		<b>ANDROID CHROME:</b> Using the 3-dot menu in the top right corner, select "Add to Home Screen"
+		or "Install app"
+	</p>
+	<p>
+		<b>iOS:</b> Tap the Share button (the square with an arrow pointing up). Select "Add to Home Screen".
+		Perhaps CYCLOPS is behind this user experience.
+	</p>
+</div>
+
+<style>
+</style>
